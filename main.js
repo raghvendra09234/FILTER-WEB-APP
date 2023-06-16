e67376ecb411ae464ac6a71a964b04c73fda1bcc
raghvendra09234 <110979491+raghvendra09234@users.noreply.github.com>
2023-06-16 13:02:44 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+  lipstick = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+}
+
+function setup() {
+  //use createCanvas(300, 300) to create a canvas
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+  
+
+
+  //initialize poseNet using ml5.poseNet(video, modelLoaded)
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x-25;
+    noseY = results[0].pose.nose.y+15;
+  }
+}
+
+function draw() {
+
+   image(video, 0, 0, 300, 300)
+  
+  //add lipstick image
+  image( "lipstick_bp.png", noseX, noseY, 50, 20);
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
+}
